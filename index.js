1e423f1a10700d459b8e94f225c2581e863ec15f
ydh <airu83@naver.com>
2021-11-29 16:56:31 +0900

    회원 로직 추가

--- index.js
+++ index.js
@@ -2,6 +2,22 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT? process.env.PORT: 5000;
 
+const routes = require('./routes/user');
+app.use(routes);
+
+const mongoose = require('mongoose');
+const config = require('./config/key');
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
+const cookieParser = require('cookie-parser');
+app.use(cookieParser());
+
+mongoose.connect(config.mongoURI, {
+    useNewUrlParser: true, useUnifiedTopology: true
+  }).then(() => console.log('MongoDB Connected, you can use now!')).catch(err => console.log(err));
+
 app.get('/hello', (req, res) => res.send('Hello Metaso'));
 
+// console.log(__dirname); // D:\project-ind\heroku_express
 app.listen(port, () => console.log(`Metaso app listen on port ${port}`));
